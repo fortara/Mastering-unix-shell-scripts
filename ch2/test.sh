a96374cb353254d45270e698294e405a35c6737c
Andrew <crazice@hotmail.com>
2012-05-15 11:11:26 +0800

    add a new functon in ch2

--- ch2/test.sh
+++ ch2/test.sh
@@ -17,5 +17,24 @@ function while_read_line_bottom
 		echo $line
 	done<$FILE
 }
-time while_read_line
-time while_read_line_bottom
+
+function while_read_line_fd
+{
+	exec 3<&0
+	exec 0<$FILE
+	while read line
+	do
+		echo $line
+	done
+	exec 0<&3
+}
+
+echo "test while_read_line"
+time while_read_line|grep time|grep -v "^$"
+echo
+echo "test while_read_line_bottom"
+time while_read_line_bottom|grep time|grep -v "^$"
+echo
+echo "test while_line_line_fd"
+time while_read_line_fd|grep time|grep -v "^$"
+echo
